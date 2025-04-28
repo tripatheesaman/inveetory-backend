41a9d228cce6de026e73b36be5df44354fb71ae5
Saman Tripathee <tripatheesaman1@gmail.com>
2025-04-28 14:29:00 +0545

    Finished request system frontend now printing left

--- src/routes/requestRoutes.ts
+++ src/routes/requestRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRequest, getPendingRequests, getRequestItems } from '../controllers/requestController';
+import { createRequest, getPendingRequests, getRequestItems, updateRequest, approveRequest, rejectRequest, getRequestById } from '../controllers/requestController';
 import verifyJWT from '../middlewares/verifyJWT';
 
 const router = express.Router();
@@ -7,5 +7,9 @@ const router = express.Router();
 router.post('/create', verifyJWT, createRequest);
 router.get('/pending', verifyJWT, getPendingRequests);
 router.get('/items/:requestNumber', verifyJWT, getRequestItems);
+router.put('/:requestNumber', verifyJWT, updateRequest);
+router.put('/:requestNumber/approve', verifyJWT, approveRequest);
+router.put('/:requestNumber/reject', verifyJWT, rejectRequest);
+router.get('/:id', verifyJWT, getRequestById);
 
 export default router; 
